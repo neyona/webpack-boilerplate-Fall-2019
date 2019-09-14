87f49d6aef64ce7bbfc5ac280dbd47afc3256266
neyona <40152041+neyona@users.noreply.github.com>
2019-09-14 13:29:11 -0400

    Changed App Index configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 const App = () => {
   return (
     <div>
-      <p>This is the App file in the source folder.</p>
+      <p>This text is in the App file in the source (src) folder.</p>
+      <p>This webpack.config.js file is set up so that HTML, CSS, Images, and
+        routing should work without anything else needing to be added. You may
+        need to change App from a const to a function if using react-router-dom
+        on this page.
+      </p>
     </div>
   );
 };
 
 export default App;
-
-ReactDOM.render(<App />, document.getElementById("app"));
